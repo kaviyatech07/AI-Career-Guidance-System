7532e9bfb9bf088a57932506713ce8d0480bb2c7
kaviyatech07 <kaviyaselvakumar07@gmail.com>
2026-06-22 12:08:59 +0530

    Create careers.py

---
+++ src/src/careers.py
@@ -0,0 +1,19 @@
+careers = {
+    "AI": [
+        "AI Engineer",
+        "Machine Learning Engineer",
+        "Computer Vision Engineer"
+    ],
+
+    "Data Science": [
+        "Data Scientist",
+        "Data Analyst",
+        "Business Analyst"
+    ],
+
+    "Software Development": [
+        "Software Developer",
+        "Full Stack Developer",
+        "Backend Developer"
+    ]
+}
