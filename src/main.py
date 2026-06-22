fbb4c9132436eb0dd2453fe74d7fcbadf97bcb7a
kaviyatech07 <kaviyaselvakumar07@gmail.com>
2026-06-22 11:10:44 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+print("===================================")
+print(" AI CAREER GUIDANCE SYSTEM ")
+print("===================================")
+
+name = input("Enter your name: ")
+
+print("Welcome,", name)
+print("Let's explore career opportunities in AI and Data Science!")
